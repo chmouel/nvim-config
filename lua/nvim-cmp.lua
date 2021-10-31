6cff8f1d688669fa3928161d26e5926d97bc26a1
Chmouel Boudjnah <chmouel@chmouel.com>
2021-10-31 09:50:21 +0100

    dunno what is this
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

---
+++ lua/nvim-cmp.lua
@@ -0,0 +1,24 @@
+-----------------------------------------------------------
+-- Autocomplete configuration file
+-----------------------------------------------------------
+
+-- Plugin: nvim-cmp
+--- https://github.com/hrsh7th/nvim-cmp
+require('luasnip/loaders/from_vscode').lazy_load()
+local cmp = require 'cmp'
+cmp.setup {
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<S-Tab]>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item()
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+  },
+}
